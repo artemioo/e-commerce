163dbc71663a0981fc576d524230dadab9190df6
artemioo <artemryb1@gmail.com>
2022-06-11 13:37:45 +0300

    created a new app accounts and login/logout/register features

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import ProductView, CategoryView, Categorize, ProductDetail
 
 urlpatterns = [
-    path('', views.main)
+    path('', ProductView.as_view(), name='home'),
+    path('categories/', CategoryView.as_view(), name='categories'),
+    path('<slug:slug>/products/', Categorize.as_view(), name='categorize'),
+    path('<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,39 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from .models import Product
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
-def main(request):
-    product = Product.objects.all()
-    return render(request, 'shop/index.html', {'product': product})
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.contrib import messages
+from .models import Product, Category
+
+
+
+class ProductView(ListView):
+    """ Главная страница """
+    model = Product
+    queryset = Product.objects.filter(available=True)
+    template_name = 'base.html'
+
+
+class CategoryView(ListView):
+    """Список категорий"""
+    model = Category
+    template_name = 'shop/category_list.html'
+
+
+class Categorize(DetailView):
+    """Продукты определенной категории """
+    model = Category
+    slug_field = 'slug'
+    template_name = 'shop/categorize.html'
+
+
+class ProductDetail(DetailView):
+    """ Информация о продукте """
+    model = Product
+    slug_field = 'slug'
+    template_name = 'shop/product_detail.html'
+
+
