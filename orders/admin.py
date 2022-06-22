7ee3587c01a45441ffa40e664ab59f6245a88789
artemioo <artemryb1@gmail.com>
2022-06-22 12:04:59 +0300

    added admin actions, product available or not, paid or not

--- orders/admin.py
+++ orders/admin.py
@@ -11,5 +11,29 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'city', 'address', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
+    actions = ['paid', 'not_paid']
     inlines = [OrderItemInline]
 
+    def not_paid(self, request, queryset):
+        """ Товара не оплачен"""
+        row_update = queryset.update(paid=False)
+        if row_update == '1':
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f'{row_update} записей было обновлено'
+        self.message_user(request, f'{message_bit}')
+
+    def paid(self, request, queryset):
+        """ Товар оплачен """
+        row_update = queryset.update(paid=True)
+        if row_update == '1':
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f'{row_update} записей было обновлено'
+        self.message_user(request, f'{message_bit}')
+
+    not_paid.short_description = 'Не оплачен'
+    not_paid.allowed_permissions = ('change', )
+
+    paid.short_description = 'Оплачен'
+    paid.allowed_permissions = ('change', )
--- shop/admin.py
+++ shop/admin.py
@@ -18,11 +18,33 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'available')
     list_filter = ('available', )
     search_fields = ('title', 'category__name')
+    actions = ['available', 'unavailable']
     form = ProductAdminForm
     prepopulated_fields = {'slug': ('name',)}
 
+    def unavailable(self, request, queryset):
+        """ Товара нет в наличии или он недоступен"""
+        row_update = queryset.update(available=False)
+        if row_update == '1':
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f'{row_update} записей было обновлено'
+        self.message_user(request, f'{message_bit}')
 
+    def available(self, request, queryset):
+        """ Товара в наличии """
+        row_update = queryset.update(available=True)
+        if row_update == '1':
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f'{row_update} записей было обновлено'
+        self.message_user(request, f'{message_bit}')
 
+    unavailable.short_description = 'Нет в наличии'
+    unavailable.allowed_permissions = ('change', )
+
+    available.short_description = 'В наличии'
+    available.allowed_permissions = ('change', )
 
 
 admin.site.register(Category)
